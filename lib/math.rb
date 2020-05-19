e32437bc7d9cb75b987c575920e3a9a5b3e80cce
Eduardo Gonzalez <edgonzalezhernandez9@gmail.com>
2020-05-19 19:14:11 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,13 +3,18 @@ def addition(num1, num2)
   puts sum
 end
 
-def substraction (num1, num2)
+def substraction(num1, num2)
   sum = num1 - num2
   puts sum
 end
 
-def division (num1, num2)
-  product = num1
+def division(num1, num2)
+  product = num1/num2
+  puts product
+end
+
+def multiplication(num1, num2)
+  
 
 
 
