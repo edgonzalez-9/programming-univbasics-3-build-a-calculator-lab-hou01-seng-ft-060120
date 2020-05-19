e56ff93fc7a7d56051bd91d4bf354b43cfb1020d
Eduardo Gonzalez <edgonzalezhernandez9@gmail.com>
2020-05-19 19:34:35 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -18,14 +18,12 @@ def multiplication(num1, num2)
   puts prodcut
 end
 
+def modulo(num1, num2)
+  product = num1 % num2
+  puts product
+end
 
 def square_root(num)
   product = math.sqrt(num)
   puts product
-end
-
-
-
-def modulo(num1, num2)
-
 end
