b1e0091be24201296c8c25b158b34425473c70dd
Eduardo Gonzalez <edgonzalezhernandez9@gmail.com>
2020-05-19 19:47:17 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,24 +7,29 @@ end
 def substraction(num1, num2)
   substraction_result = num1 - num2
   puts substraction_result
+  substraction_result
 end
 
 def division(num1, num2)
   division result = num1/num2
   puts division result
+  division_result
 end
 
 def multiplication(num1, num2)
   multiplication_result = num1*num2
   puts multiplication_result
+  multiplication_result
 end
 
 def modulo(num1, num2)
   modulo_result = num1 % num2
   puts modulo_result
+  modulo_result
 end
 
 def square_root(num)
   square_root_product = Math.sqrt(num)
   puts square_root_product
+  square_root_result
 end
